aaa475246ddf32f09596da4e590434c4a902543d
kz56cd <kz56cd@gmail.com>
2020-05-21 08:31:38 +0900

    Add tab bar, and child view controllers

---
+++ storyboard/skelton/View/Common/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  skelton
+//
+//  Created by Masakazu Sano on 2020/05/21.
+//  Copyright © 2020 kz56cd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ storyboard/skelton/View/List/ListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ListViewController.swift
+//  skelton
+//
+//  Created by Masakazu Sano on 2020/05/21.
+//  Copyright © 2020 kz56cd. All rights reserved.
+//
+
+import UIKit
+
+final class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
